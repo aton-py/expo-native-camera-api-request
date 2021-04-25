5df6198d4b8bb3f13da02652b57a2b805c09fd35
Aton <dornfeld.in@live.com>
2021-04-25 14:54:45 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,23 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import Routes from './src/Routes';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <Routes />
+  );
 }
---
+++ src/Routes.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native';
+import Camera from './pages/Camera';
+import Main from './pages/Main';
+
+const Tab = createBottomTabNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          options={{
+            tabBarIcon: ( { color } ) => <Feather name="home" size={20} color={color} /> 
+            }} 
+            name="Main" 
+            component={Main} 
+            />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ( { color } ) => <Feather name="camera" size={20} color={color} /> 
+            }} 
+            name="Camera" 
+            component={Camera} 
+            />
+        </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
